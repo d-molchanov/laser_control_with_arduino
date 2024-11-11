bfda48d594412b2d4711e565d01ada95e9acfe09
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2024-11-11 18:21:19 +0300

    Working sketch for three modes added.

--- sketch/sketch_for_laser.ino
+++ sketch/sketch_for_laser.ino
@@ -1,71 +1,182 @@
-// Arduino таймер CTC прерывание
-// avr-libc library includes
-#include <avr/io.h>
-#include <avr/interrupt.h>
+#include <TimerOne.h>
 #define GENPIN 2
+// #define GENPIN 12
+// Message structure: "p4095:00010000:00010000:00200"
 
-const int numPins = 13;
-int inputValue;
 
-void setup()
-{
+bool singlePulseModeUS;
+bool singlePulseModeMS;
+// volatile int periodicModeUS;
+bool continuousMode;
+bool periodicModeUS;
+bool periodicModeMS;
 
-    Serial.begin(9600);
-    for (int i = 2; i < 13; i++) {
-      pinMode(i, OUTPUT);
+bool timerRunning = false;
+long i;
+long count;
+
+long intensity;
+long pulseDuration;
+long pauseDuration;
+long iterations;
+
+void generatePulseUS(long pulse, long pause) {
+  digitalWrite(GENPIN, HIGH);
+  // PORTB |= (1 << 4);
+  delayMicroseconds(pulse);
+  digitalWrite(GENPIN, LOW);
+  // PORTB &= ~ (1 << 4);
+  delayMicroseconds(pause);
+}
+
+void generatePulseMS(long pulse, long pause) {
+  digitalWrite(GENPIN, HIGH);
+  // PORTB |= (1 << 4);
+  delay(pulse);
+  digitalWrite(GENPIN, LOW);
+  // PORTB &= ~ (1 << 4);
+  delay(pause);
+}
+
+void setup() {
+  // Начинаем работу с Serial портом на скорости 9600 бит/с
+  Serial.begin(9600);
+  Serial.setTimeout(50);
+  for (int i = 2; i < 13; i++) {
+    pinMode(i, OUTPUT);
+  }
+  delay(1000);
+  controlPins(3);
+}
+
+void loop() {
+  if (periodicModeUS) {
+    if (i < iterations) {
+      generatePulseUS(pulseDuration, pauseDuration);
+      i++;
+    } else {
+      periodicModeUS = false;
     }
-    int fr = 200;
-    float res = 1.6e-5;
-    int c = ceil(1.0/fr/res - 1);
-    Serial.println("Частота на пине " + String(GENPIN) + ": " + String(fr) + " Гц");
-    Serial.println("Значение регистра совпадения: " + String(c));
-    // pinMode(GENPIN, OUTPUT);
-    // инициализация Timer1
-    cli();  // отключить глобальные прерывания
-    TCCR1A = 0;   // установить регистры в 0
-    TCCR1B = 0;
-
-    // OCR1A = 15624; // установка регистра совпадения
-    // OCR1A = 624; // установка регистра совпадения
-    OCR1A = c; // установка регистра совпадения
-    
-    TCCR1B |= (1 << WGM12);  // включить CTC режим 
-    // TCCR1B |= (1 << CS10); // Установить биты на коэффициент деления 1024
-    // TCCR1B |= (1 << CS12);
-    TCCR1B |= (1 << CS12); // Установить биты на коэффициент деления 256
-
-    TIMSK1 |= (1 << OCIE1A);  // включить прерывание по совпадению таймера 
-    sei(); // включить глобальные прерывания
-}
-
-void loop()
-{
-    // основная программа
-    if (Serial.available() > 0){
-      int n = Serial.parseInt();
-      if (n < 3) {
-        // Serial.println(n);
-        n = 3;
-      }
-      if (n > 4095) {
-        // Serial.println(n);
-        n = 4095;
-      }
-      Serial.println(n);
-      for (int i = 2; i < 12; i++){
-        if ((n >> i) & 1) {
-          digitalWrite(14 - i, HIGH);
-          // Serial.println("Pin " + String(14-i) + ": 1");
-        } else {
-          digitalWrite(14 - i, LOW);
-          // Serial.println("Pin " + String(14-i) + ": 0");
-        }
-        Serial.println("Pin " + String(14 - i) + ": " + digitalRead(14-i));
-      }
+  }
+  if (periodicModeMS) {
+    if (i < iterations) {
+      generatePulseMS(pulseDuration, pauseDuration);
+      i++;
+    } else {
+      periodicModeMS = false;
     }
+  }
+  if (continuousMode) {
+    digitalWrite(GENPIN, HIGH);
+    continuousMode = false;
+  }
+  if (singlePulseModeUS) {
+    generatePulseUS(pulseDuration, pauseDuration);
+    singlePulseModeUS = false;
+  }
+  if (singlePulseModeMS) {
+    generatePulseMS(pulseDuration, pauseDuration);
+    singlePulseModeMS = false;
+  }
+  if (Serial.available() > 0) {
+    digitalWrite(GENPIN, LOW);
+    count = 0;
+    String receivedData = Serial.readString();
+    String mode = receivedData.substring(0, 1);
+    if (mode == "t") {
+      controlPins(3);
+      singlePulseModeUS = false;
+      singlePulseModeMS = false;
+      periodicModeUS = false;
+      periodicModeMS = false;
+      continuousMode = false;
+      pulseDuration = 0;
+      pauseDuration = -1;
+      iterations = 0;
+    } else {
+      intensity = receivedData.substring(1, 5).toInt();
+      pulseDuration = receivedData.substring(6, 14).toInt();
+      pauseDuration = receivedData.substring(15, 23).toInt();
+      iterations = receivedData.substring(24, 31).toInt();
+      controlPins(intensity);
+      if (mode == "s") {
+        singlePulseModeUS = true;
+        singlePulseModeMS = false;
+        periodicModeUS = false;
+        periodicModeMS = false;
+        continuousMode = false;
+        iterations = 1;
+      }
+      if (mode == "l") {
+        singlePulseModeUS = false;
+        singlePulseModeMS = true;
+        periodicModeUS = false;
+        periodicModeMS = false;
+        continuousMode = false;
+        iterations = 1;
+      }
+      if (mode == "p") {
+        singlePulseModeUS = false;
+        singlePulseModeMS = false;
+        periodicModeUS = true;
+        periodicModeMS = false;
+        continuousMode = false;
+        timerRunning = true;
+        i = 0;
+      }
+      if (mode == "m") {
+        singlePulseModeUS = false;
+        singlePulseModeMS = false;
+        periodicModeUS = false;
+        periodicModeMS = true;
+        continuousMode = false;
+        timerRunning = true;
+        i = 0;
+      }
+      if (mode == "c") {
+        singlePulseModeUS = false;
+        singlePulseModeMS = false;
+        periodicModeUS = false;
+        periodicModeMS = false;
+        continuousMode = true;
+        iterations = -1;
+      }
+      // Serial.println("Mode: " + mode);
+      // Serial.println("Intensity: " + String(intensity));
+      // Serial.println("Pulse duration: " + String(pulseDuration) + " us");
+      // Serial.println("Pause duration: " + String(pauseDuration) + " us");
+      // Serial.println("Iterations: " + String(iterations));
+      // Serial.println("Checked intensity: " + String(checkPins()));
+    }
+    Serial.println(mode + ";" + String(checkPins()) + ";" + String(pulseDuration) + ";" + String(pauseDuration) + ";" + String(iterations));
+  }
+
 }
 
-ISR(TIMER1_COMPA_vect)
-{
-    digitalWrite(GENPIN, !digitalRead(GENPIN));
+void controlPins(int value) {
+  if (value < 3) {
+    value = 3;
+  }
+  if (value > 4095) {
+    value = 4095;
+  }
+  for (int i = 2; i < 12; i++) {
+    if ((value >> i) & 1) {
+      digitalWrite(14 - i, HIGH);
+    } else {
+      digitalWrite(14 - i, LOW);
+    }
+  }
 }
+
+int checkPins() {
+  int result = 0;
+  for (int i = 3; i < 13; i++) {
+    result <<= 1;
+    result += digitalRead(i);
+  }
+  result <<= 2;
+  result += 3;
+  return result;
+}
+
